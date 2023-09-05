08450884fe39083a48b314e8e0fb5e55e16e52d7
Marcus Liu <liu.mingku@northeastern.edu>
2023-09-05 17:12:54 -0400

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by 柳铭坤 on 2023/9/5.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
